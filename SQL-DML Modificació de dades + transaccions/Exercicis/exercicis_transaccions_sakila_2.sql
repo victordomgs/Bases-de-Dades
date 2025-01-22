914a5241d56c130676122466a6db66a309d73318
victordomgs <victor.garcia.saiz@gmail.com>
2025-01-22 18:24:28 +0100

    Create exercicis_transaccions_sakila_2.sql

---
+++ SQL-DML Modificació de dades + transaccions/Exercicis/exercicis_transaccions_sakila_2.sql
@@ -0,0 +1,15 @@
+/*******************************************************************************
+   Utilitzant la base de dades sakila, realitzeu els següents exercicis amb TRANSACTIONS:
+********************************************************************************/
+
+/*Augmenta el preu de lloguer de les pel·lícules de més durada: Augmenta en 2.00 el preu ('rental_rate') de totes les pel·lícules que tenen una durada superior a 150 minuts.*/
+
+/*Marca com a inactius ('active' = 0) tots els clients que van ser creats fa més de 5 anys i a que a més no tenen cap lloguer actiu.*/
+
+/*Modifica la classificació ('rating') de les pel·lícules per a una classificació més restrictiva: Totes les pel·lícules amb classificació PG es canvien a 'PG-13'.*/
+
+/*Afegeix un nou client amb una adreça fictícia.*/
+
+/*Crea una nova ciutat amb el seu districte.*/
+
+/*Afegeix una nova pel·lícula al catàleg.*/
