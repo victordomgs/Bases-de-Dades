b3a5669b9ec0fc769866c3511999b6a1671e51c1
victordomgs <victor.garcia.saiz@gmail.com>
2025-01-23 10:24:01 +0100

    Create exercicis_transaccions_sakila_3.sql

---
+++ SQL-DML Modificació de dades + transaccions/Exercicis/exercicis_transaccions_sakila_3.sql
@@ -0,0 +1,65 @@
+/*******************************************************************************
+   Utilitzant la base de dades sakila, realitzeu els següents exercicis amb TRANSACTIONS:
+********************************************************************************/
+
+
+/*Exercicis creats per Joan Queralt: https://gitlab.com/joanq/DAM-M2-BasesDeDades/-/blob/master/UF2/1-DML/activitats/activitat_modificacio_sakila.adoc?ref_type=heads*/
+
+/*Per a cada qüestió, indica totes les sentències que has necessitat per a realitzar la modificació necessària. Si es necessiten consultes intermèdies per resoldre una qüestió, cal guardar els valors necessaris en variables, o utilitzar subconsultes.*/
+
+
+/*1. Afegeix una nova actriu a la BD amb el nom Charlize Theron.*/
+
+/*2. Afegeix la pel·licula Monster a la BD, de l’any 2003 i amb les mateixes característiques que la pel·lícula BOOGIE AMELIE.*/
+
+/*3. Afegeix l’actriu del primer exercici a la pel·lícula del segon exercici.*/
+
+/*4. Afegeix la pel·lícula Monster a la categoria de Drama.*/
+
+/*5. Afegeix una nova pel·lícula, la que vulguis, amb les mateixes dades que la pel·lícula que tingui menys durada.*/
+
+/*6. Insereix una nova categoria amb el nom Noir.*/
+
+/*7. Afegeix un nou client de la ciutat on hi hagi més clients. La resta de dades les pots inventar.*/
+
+/*8. Fes les operacions que calgui per tal d’afegir un nou lloguer a aquest client.*/
+
+/*9. Augmenta en dos minuts la durada de les pel·lícules de la categoria d’acció.*/
+
+/*10. Canvia l’adreça de la clienta Carol Garcia. La seva nova adreça és 150 Blue Avenue enlloc de 320 Brest Avenue.*/
+
+/*11. Disminueix un 10% la quantitat dels pagaments fets al juliol del 2005.*/
+
+/*12. Esborra el client o els clients que hagin fet només un lloguer.*/
+
+/*13. Esborra la pel·lícula que dura menys de la BD.*/
+
+/*14. Esborra la categoria Travel.*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
