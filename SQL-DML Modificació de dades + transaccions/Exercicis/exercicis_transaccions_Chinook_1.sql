8874141cca68b02ff2b9a1d4a56473323a5f39c8
victordomgs <victor.garcia.saiz@gmail.com>
2025-01-31 16:00:42 +0100

    Create exercicis_transaccions_Chinook_1.sql

---
+++ SQL-DML Modificació de dades + transaccions/Exercicis/exercicis_transaccions_Chinook_1.sql
@@ -0,0 +1,34 @@
+/*******************************************************************************
+   Utilitzant la base de dades Chinook, realitzeu els següents exercicis amb TRANSACTIONS:
+********************************************************************************/
+
+
+/*Exercicis creats per Joan Queralt: https://gitlab.com/joanq/DAM-M2-BasesDeDades/-/blob/master/UF2/1-DML/activitats/activitat_modificacio_sakila.adoc?ref_type=heads*/
+/*Per a cada qüestió, indica totes les sentències que has necessitat per a realitzar la modificació necessària. Si es necessiten consultes intermèdies per resoldre una qüestió, cal guardar els valors necessaris en variables, o utilitzar subconsultes.*/
+
+
+/*1. Afegeix-te com a client a la botiga. No cal que posis dades reals, a part del teu nom i cognom, però has d’omplir correctament tots els camps.*/
+
+/*2. Volem introduir un nou artista i algun dels seus àlbums a la base de dades. L’artista es diu M.I.A. i l’àlbum que volem introduir és Kala. Pots consultar la llista de les cançons a https://en.wikipedia.org/wiki/Kala_%28album%29.*/
+
+/*3. Crea una nova llista de reproducció i afegeix-hi almenys 5 cançons (que ja estiguin a la base de dades).*/
+
+/*4. El client Niklas Schröder ha fet una nova compra a la nostra botiga. Ha adquirit dos exemplars de la cançó Losing My Religion i un exemplar de la cançó Heartland. Introdueix les dades corresponents a les taules Invoice i InvoiceLine, suposant que ha facturat a la seva adreça habitual.*/
+
+/*5. Hem contractat al client John Gordon. El càrrec que ocuparà serà el de Sales Manager i el seu cap serà la Nancy Edwards. Afegeix una nova fila a la taula d’empleats, copiant totes les dades possibles de la taula de clients.*/
+
+/*6. El client Mark Taylor s’ha canviat d’adreça. Ara viu a 68-70 Oxford St, Darlinghurst NSW 2010, Australia. Modifica la seva entrada a la base de dades d’acord amb aquest canvi.*/
+
+/*7. Disminueix un 20% els preus de les 30 cançons més curtes de la base de dades.*/
+
+/*8. Augmenta un 15% els preus de les 20 cançons més venudes de la base de dades.*/
+
+/*9. Fins ara, els clients de la Índia tenien com a assistent a l’empleada Jane Peacock, però a partir d’ara tindran a la Margaret Park. Fes el canvi corresponent a la base de dades.*/
+
+/*10. El client Eduardo Martins s’ha queixat d’un error a la seva última factura. A més de les cançons que hi consten, assegura que també va comprar un exemplar de la cançó Garota De Ipanema. Modifica la seva última factura per afegir aquesta compra.*/
+
+/*11. Un error ha fet que totes les factures emeses el dia 28 de gener de 2013 siguin errònies: es van guardar a la base de dades, però no es van cobrar ni es van entregar les cançons. Elimina totes les factures emeses aquest dia.*/
+
+/*12. Sembla que hi ha dues llistes de reproducció repetides a la base de dades. Són les que tenen com a nom Music. Elimina una de les dues llistes de reproducció completament.*/
+
+/*13. Esborra totes les cançons de la base de dades que no s’hagin venut cap vegada fins ara.*/
