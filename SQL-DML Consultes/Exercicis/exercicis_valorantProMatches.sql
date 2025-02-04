2712c505e529b60ca2a2ba339dc249c4dfaefd84
victordomgs <victor.garcia.saiz@gmail.com>
2025-02-04 14:53:56 +0100

    Update and rename exercicis_valorantProMatches_subconsultes.sql to exercicis_valorantProMatches.sql

---
+++ SQL-DML Consultes/Exercicis/exercicis_valorantProMatches.sql
@@ -0,0 +1,13 @@
+/*******************************************************************************
+   Utilitzant la base de dades valorantProMatches, realitzeu els següents exercicis sobre consultes:
+********************************************************************************/
+
+/*Mostra el nom de l'equip que més victòries té? Taules: 'teams' i 'games'*/
+
+/*Quin és el jugador amb més eliminacions (kills) en una única partida? Indica el nom del jugador (com a player_name). Taules: player_stats, players*/
+
+/*Quin és el nom de l'agent més utilitzat a tots els partits? Taules: agets, player_stats*/
+
+/*Quins són els distints equips que han jugat al mapa més popular? Taules: games, maps, teams*/
+
+/*Quins és l'esdeveniment (eventName) que ha tingut més partits? Taules: events, matches*/
