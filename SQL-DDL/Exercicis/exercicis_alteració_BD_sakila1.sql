7235d157891468602e946f24feb4c3f189647f2c
victordomgs <victor.garcia.saiz@gmail.com>
2025-02-16 18:48:08 +0100

    Create exercicis_alteració_BD_sakila1.sql

---
+++ SQL-DDL/Exercicis/exercicis_alteració_BD_sakila1.sql
@@ -0,0 +1,13 @@
+/*******************************************************************************
+   Utilitzant la base de dades sakila, realitzeu els següents exercicis sobre consultes:
+********************************************************************************/
+
+/*Afegeix una columna phone_number de tipus VARCHAR(15) a la taula customer per poder emmagatzemar el número de telèfon dels clients. Aquesta columna ha de permetre valors nuls.*/
+
+/*La columna amount de la taula payment és de tipus DECIMAL(5,2). Suposa que cal augmentar la precisió per permetre valors més grans. Modifica la columna perquè sigui DECIMAL(7,2).*/
+
+/*La taula rental té una columna staff_id que referencia la columna staff_id de la taula staff. Afegeix una clau forana per garantir la integritat referencial.*/
+
+/*La taula actor té una columna anomenada middle_name que ja no és necessària. Elimina aquesta columna.*/
+
+/*Renombra la taula inventory a stock per adequar millor el nom a la seva funcionalitat.*/
