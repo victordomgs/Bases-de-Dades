ec27fa4b9802c6684fc0b2a68d88282ff17a9a0b
victordomgs <victor.garcia.saiz@gmail.com>
2025-02-16 18:56:05 +0100

    Create exercicis_vistes_BD_valorantProMatches.sql

---
+++ SQL-DDL/Exercicis/exercicis_vistes_BD_valorantProMatches.sql
@@ -0,0 +1,13 @@
+/*******************************************************************************
+   Utilitzant la base de dades valorantProMatches, realitzeu els següents exercicis sobre vistes:
+********************************************************************************/
+
+/*Crea una vista anomenada player_basic_stats que mostri el player_id, el game_id, els kills, les deaths i els assists de la taula Player_Stats.*/
+
+/*Crea una vista anomenada games_winners que mostri el game_id, el winner i el nom de l’equip guanyador (de la taula Teams).*/
+
+/*Crea una vista anomenada games_high_rounds que mostri el game_id, team1_id, team2_id, team1_TotalRounds i team2_TotalRounds de les partides on algun dels dos equips ha guanyat més de 13 rondes.*/
+
+/*Crea una vista anomenada games_overtime que mostri el game_id, team1_id, team2_id, Team1_RoundsOT i Team2_RoundsOT de les partides on s’ha jugat pròrroga (és a dir, alguna de les dues columnes de RoundsOT és més gran que 0).*/
+
+/*Crea una vista anomenada top_performers que mostri el player_id, game_id, acs, kills i deaths dels jugadors que tinguin un acs superior a 250.*/
